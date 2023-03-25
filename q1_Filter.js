71aeca446070ca7adc0d6e763da5cd6070a4e9d3
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-25 08:30:57 +0530

    day 7 first quest using filter method in xml request of rest api

---
+++ q1_Filter.js
@@ -0,0 +1,24 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/all', true);
+request.responseType = 'json';
+
+request.onload = function () {
+
+    var responseObj = request.response
+    var res = responseObj.filter((item) => {
+
+        return item.continents == 'Asia';
+    })
+
+
+    console.log(res.map((item) => {
+        return item.name.common
+    }));
+
+
+};
+request.onerror = function () {
+    console.log('Error in connecting api');
+
+}
+request.send();
